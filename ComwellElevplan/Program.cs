7af4bbb665a86596314733e618318b1a65e102b7
Alex <aleexskov@gmail.com>
2025-05-23 16:00:34 +0200

    Fixede azure 1

--- ComwellElevplan/Program.cs
+++ ComwellElevplan/Program.cs
@@ -9,3 +9,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+builder.Services.AddScoped(sp =>
+{
+    var baseUri = builder.HostEnvironment.IsDevelopment()
+        ? "https://localhost:5102/" // ← din API-port lokalt
+        : builder.HostEnvironment.BaseAddress; // ← på Azure
+
+    return new HttpClient { BaseAddress = new Uri(baseUri) };
+});
