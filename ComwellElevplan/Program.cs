fe10e3136f01957b6b23b7ea14ac265b7164e7fb
Alex <aleexskov@gmail.com>
2025-05-27 09:28:45 +0200

    Fix azure plz plz  plZZZZ

--- ComwellElevplan/Program.cs
+++ ComwellElevplan/Program.cs
@@ -8,8 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
 
 builder.Services.AddScoped(sp =>
